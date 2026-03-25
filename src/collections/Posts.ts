5c603d04f7dfb902206c3227669e7e81ba2fd3df
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-25 14:01:49 +0530

    feat: implement Posts collection configuration

---
+++ src/collections/Posts.ts
@@ -0,0 +1,23 @@
+import { CollectionConfig } from 'payload'
+
+export const Posts: CollectionConfig = {
+  slug: 'posts', // The slug is the unique identifier for the collection and is used in the API routes
+  admin: {
+    useAsTitle: 'title',
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'content',
+      type: 'textarea',
+    },
+    {
+      name: 'publishedAt',
+      type: 'date',
+    },
+  ],
+}
